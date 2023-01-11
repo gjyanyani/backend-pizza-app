176dbb87df93423de255fac01d7361536bd899e4
Girish Kumar <gjyanyani@gmail.com>
2023-01-11 13:17:02 +0530

    get api res send testing

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const express= require('express');
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to our online shop API...");
+});
+
+app.get("/products", (req, res) => {
+  res.send([2, 3, 4]);
+});
+
+const port = process.env.PORT || 5425;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}  `);
+});
